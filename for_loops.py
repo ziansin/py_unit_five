1cda70f9c6e74caf68f6190f50c75127eb743d36
ziansin <pilchza@ssfs.org>
2022-11-03 11:30:12 -0400

    count

--- for_loops.py
+++ for_loops.py
@@ -1,5 +1,9 @@
 
 def count(first, last):
+    test = ""
+    for x in range(first, last):
+        test += str(x) + " "
+    return(test)
     """
     This function will create a string of numbers separated by a space. The numbers will start with the
     first number and end with the second. The second number SHOULD be included as part of the string. If
@@ -9,8 +13,6 @@ def count(first, last):
     :param second: The final number. Must be included
     :return: A string containing the numbers
     """
-    pass # make sure to delete this line when you write your own function
-
 
 def main():
     print(count(0, 6))
