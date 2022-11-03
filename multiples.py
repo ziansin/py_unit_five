395c38d5b9fb6d54776112ed344584db750c4828
ziansin <pilchza@ssfs.org>
2022-11-03 11:41:17 -0400

    multiplication

--- multiples.py
+++ multiples.py
@@ -5,6 +5,7 @@ def get_sum(number):
     :param number: The value up to which we want to find the sum. Not included in the calculation
     :return: The sum of all the multiples of 3 or 5 up to the number.
     """
-    pass # make sure to delete this line when writing your function
+
+
 
 
--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,16 @@
 def multiplication_table(number):
+    test = ""
+    for x in range(number, number * 13, number):
+        test += str(x) + " "
+    return(test)
     """
     Ex. multiplication_table(6) returns "6 12 18 24 30 36 42 48 54 60 66 72 "
     :param number: An integer
     :return: A string of 12 values representing the mulitiplication table of the parameter number.
     """
-    pass # Make sure to delete this line when writing your function.
+
+def main():
+        print(multiplication_table(6))
+
+if __name__ == '__main__':
+        main()
